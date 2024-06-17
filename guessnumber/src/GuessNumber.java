6402e39d21674a2990785d393b75baa3940b77ad
Rexy998 <159923347+Rexy998@users.noreply.github.com>
2024-06-17 00:42:50 -0400

    Add files via upload

--- guessnumber/src/GuessNumber.java
+++ guessnumber/src/GuessNumber.java
@@ -3,21 +3,21 @@ import javax.swing.JOptionPane;
 public class GuessNumber {
     void Guess() {
   ;
-    JOptionPane.showMessageDialog(null, "Guess the number between one and one hundred");
+    JOptionPane.showMessageDialog(null, "Guess the number between one and one hundred"); //prompts user
 
-        int num = (int) (Math.random()*100);
+        int num = (int) (Math.random()*100); //creates random number
 
         while(true) {
-            int guess = Integer.parseInt(JOptionPane.showInputDialog("your guess:"));
+            int guess = Integer.parseInt(JOptionPane.showInputDialog("your guess:")); //gathers user input
             if (guess > num) {
-                JOptionPane.showMessageDialog(null, "The number is less than your guess");
+                JOptionPane.showMessageDialog(null, "The number is less than your guess"); 
                 }
             else if (guess < num){
             JOptionPane.showMessageDialog(null, "The number is more than your guess");
             }
             else {
                 JOptionPane.showMessageDialog(null, "correct guess🎉");
-                break;
+                break; //ends program upon correct guess
             }
         }
     }
