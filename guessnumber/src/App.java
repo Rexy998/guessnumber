d39a43a60d544e238fc293aab086c6952653568c
Rexy998 <159923347+Rexy998@users.noreply.github.com>
2024-06-17 00:41:50 -0400

    Add files via upload

--- guessnumber/src/App.java
+++ guessnumber/src/App.java
@@ -3,7 +3,7 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-                    GuessNumber object = new GuessNumber();
-                    object.Guess();
+                    GuessNumber object = new GuessNumber(); //creates obj of file
+                    object.Guess(); //calls method
     }
 }
